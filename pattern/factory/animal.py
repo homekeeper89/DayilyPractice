012b382d10146541f181bcd5f2a4bd50007fa3cd
Homekeeper <homekeeper89@gmail.com>
2019-06-17 00:51:18 +0900

    [new] 패턴 추가

--- pattern/factory/animal.py
+++ pattern/factory/animal.py
@@ -1,5 +1,10 @@
 from abc import ABCMeta, abstractmethod
 
+"""
+cat, dog는 animal을 상속받고 추상메서드를 구체화 하였음
+ForestFactory는 인자에 따라 Animal 인스턴스를 생성하고 사운드를 출력함
+클라이언트는 인자만 전달해주면 됨.
+"""
 class Animal(metaclass=ABCMeta):
     @abstractmethod
     def do_say(self):
