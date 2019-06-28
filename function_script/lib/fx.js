e9b61011e64fdf388199c2c460ec84a2cf121e9d
Homekeeper <homekeeper89@gmail.com>
2019-06-29 00:36:30 +0900

    [new] 총 수량, 총 가격 예시, sum을 통한 추상화 레벨 높이기

--- function_script/lib/fx.js
+++ function_script/lib/fx.js
@@ -1,6 +1,7 @@
 const log = console.log;
 const curry = f => (a, ..._) =>_.length ? f(a, ..._) : (..._)=> f(a, ..._)
-
+const go = (...args) =>reduce((a, f)=>f(a),  args)
+const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs)
 const map = curry((f, iter)=>{ // 어떤 값을 수집할지에 대해 추상화 한다(함수 f에게 위임한다), 직접 입력안함
     let res = [];
     for (const a of iter){
