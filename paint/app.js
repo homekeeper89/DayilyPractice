83fdf8f35bba4392178615e2d350b7a80dbae5a3
Homekeeper <homekeeper89@gmail.com>
2019-08-12 00:44:35 +0900

    [new] canvas context 상태를 만듬

--- paint/app.js
+++ paint/app.js
@@ -1,4 +1,13 @@
 const canvas = document.getElementById("jsCanvas")
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle="#2c2c2c";
+ctx.lineWidth=2.5;
+
+function startPainting(){
+    painting = true
+}
+
 let painting = false;
 
 function stopPainting(){
@@ -8,6 +17,13 @@ function stopPainting(){
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y)
+    }else{
+        ctx.lineTo(x, y);
+        ctx.stroke()
+    }
 }
 
 function onMouseDown(event){
@@ -21,7 +37,7 @@ function onMouseUp(event){
 
 if(canvas){
     canvas.addEventListener('mousemove',  onMouseMove)
-    canvas.addEventListener('mousedown', onMouseDown)
-    canvas.addEventListener('mouseup', onMouseUp)
+    canvas.addEventListener('mousedown', startPainting)
+    canvas.addEventListener('mouseup', stopPainting)
     canvas.addEventListener('mouseleave', stopPainting)
 }
