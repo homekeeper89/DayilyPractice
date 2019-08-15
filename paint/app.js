cfc0993f12580b3b2a88e6ee36bb35736096a21f
Homekeeper <homekeeper89@gmail.com>
2019-08-16 00:53:55 +0900

    [new] 5회차, 페인트시 굵기 조절 기능 및 컨버스 삭제 기능 추가
    - 5회차 강의 내용, canvas의 백그라운드 색상을 입혀주는 기능 추가

--- paint/app.js
+++ paint/app.js
@@ -1,18 +1,26 @@
 const canvas = document.getElementById("jsCanvas")
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor")
+const range = document.getElementById("jsRange")
+const mode = document.getElementById("jsMode")
+const INITIAL_COLOR = "#2c2c2c"
+const CANVAS_SIZE = 700;
 
-canvas.width = 700;
-canvas.height = 700;
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
 
-ctx.strokeStyle="#2c2c2c"; // 컨버스에 그리는 색상
+ctx.strokeStyle=INITIAL_COLOR; // 컨버스에 그리는 색상
+ctx.fillStyle =INITIAL_COLOR
 ctx.lineWidth=2.5;
+// ctx.fillStyle="green";
+// ctx.fillRect(50, 20, 100, 40);
 
 function startPainting(){
     painting = true
 }
 
 let painting = false;
+let filling = false;
 
 function stopPainting(){
     painting = false;
@@ -40,8 +48,28 @@ function onMouseUp(event){
 }
 function handleColorClick(event){
     const color = event.target.style.backgroundColor;
-    console.log(color)
     ctx.strokeStyle = color
+    ctx.fillStyle=ctx.strokeStyle
+}
+
+function handleRangeChange(event){
+  ctx.lineWidth = event.target.value
+}
+
+function handleModeClick(){
+  if(filling == true){
+    filling = false;
+    mode.innerText = "Fill"
+  }else{
+    filling = true;
+    mode.innerText = "Paint"
+    
+  }
+}
+function handleCanvasClick(){
+  if(filling){
+    ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE)
+  }
 }
 
 if(canvas){
@@ -49,8 +77,20 @@ if(canvas){
     canvas.addEventListener('mousedown', startPainting)
     canvas.addEventListener('mouseup', stopPainting)
     canvas.addEventListener('mouseleave', stopPainting)
+    canvas.addEventListener('click', handleCanvasClick)
 }
 
 console.log(Array.from(colors)); // Array.from은 list를 만든다
 
-Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick))
+if(colors){
+    Array.from(colors).forEach(
+        color => color.addEventListener("click", handleColorClick))
+}
+
+if(range){
+  range.addEventListener("input", handleRangeChange)
+}
+
+if(mode){
+  mode.addEventListener("click", handleModeClick)
+}
