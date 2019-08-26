5bf5a4bb0071dad6950d45ad822ddfa024693e3b
Homekeeper <homekeeper89@gmail.com>
2019-08-27 00:58:45 +0900

    [new] bubble sort 자체 구현
    버블 소트는 리스트를 순회하면서 바로 옆? n+1 자리와 비교해서 자리를 바꾼다
    그리고 한번 바뀌면 다시 앞과 확인하는 식으로...
    정답과 많이 다르다 내 답은

---
+++ basic/sort.py
@@ -0,0 +1,18 @@
+number = [5,3,8,6,7,2]
+
+def bubble_sort(num_list):
+    temp = 0
+    for index in range(1, len(num_list)):
+        for v in range(index, 0, -1):
+            min_num = num_list[v]
+            if min_num < num_list[v-1]:
+                num_list[v] = num_list[v-1]
+                num_list[v-1] = min_num
+                min_num = num_list[v]
+    return num_list
+
+
+    return num_list
+if __name__ == '__main__':
+    print(bubble_sort(number))
+
