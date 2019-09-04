b15600f50376e716fa8d3d2582274e540a6d4366
Homekeeper <homekeeper89@gmail.com>
2019-09-05 00:04:52 +0900

    [new] promise를 합성함수에 사용할때 예시인데
    비동기 중에 에러? 합성중 에러? 를 잡기위해 catch를 사용한다.. 이정도로만 이해하게 됨
    
    go함수에서 promise를 사용하려면 reduce가 돌아가는 역할에서 promise 인지 아닌지를
    잡아내는 로직을 추가해야한다. instanceof 정도로..

--- function_script/lib/fx.js
+++ function_script/lib/fx.js
@@ -5,6 +5,13 @@ const curry = f => (a, ..._) =>_.length ? f(a, ..._) : (..._)=> f(a, ..._)
 const go = (...args) =>reduce((a, f)=>f(a),  args)
 const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs)
 
+const find = curry((f, iter)=> go(
+    iter,
+    filter(f),
+    take(1),
+    ([a]) => a
+  ))
+
 const map = curry((f, iter)=>{ // 어떤 값을 수집할지에 대해 추상화 한다(함수 f에게 위임한다), 직접 입력안함
     let res = [];
     for (const a of iter){
@@ -21,8 +28,33 @@ const filter = curry((f, iter)=>{
     }
     return res
 })
+const go1 = (a,f) => a instanceof Promise ? a.then(f) : f(a);
 
 const reduce = curry((f, acc, iter)=>{
+    if(!iter){
+        iter = acc[Symbol.iterator]();
+        acc = iter.next().value;
+    }else{
+        iter = iter[Symbol.iterator]();
+    }
+    let cur;
+    // while(!(cur = iter.next()).done){
+    //     const a = cur.value;
+    //     acc = f(acc, a);
+    //     // acc = acc instanceof Promise ? acc.then(acc=>f(acc,a)):f(acc,a)
+    // }
+    return go1(function recur(acc){
+        while(!(cur = iter.next()).done){
+        const a = cur.value;
+        acc = f(acc, a);
+        // acc = acc instanceof Promise ? acc.then(acc=>f(acc,a)):f(acc,a)
+        if (acc instanceof Promise) return acc.then(recur);
+        }
+        return acc;
+    }(acc));
+})
+
+const reduce_legacy = curry((f, acc, iter)=>{
     if(!iter){
         iter = acc[Symbol.iterator]();
         acc = iter.next().value;
