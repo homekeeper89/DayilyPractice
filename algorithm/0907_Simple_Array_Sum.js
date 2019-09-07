376ff06ee95c1a10d1c85ec2e06f6fcc4ad9c848
Homekeeper <homekeeper89@gmail.com>
2019-09-07 22:31:31 +0900

    [new] 해커랭크 데일리 일주일 플랜
    일주일, 총 7일간 매일 한문제씩 푼다
    그리고 그 문제를 다음주에는 리팩토링 한다
    과연 될런지...
    
    첫번째는 arra sum이고 이를 reduce를 이용해서 풀었다

---
+++ algorithm/0907_Simple_Array_Sum.js
@@ -0,0 +1,19 @@
+
+const log = console.log
+const reduce = (f, acc, iter)=>{
+    if(!iter){
+        iter = acc[Symbol.iterator]()
+        acc = iter.next().value
+    }
+    for(const value of iter){
+        acc = f(acc, value)
+    }
+    return acc
+}
+
+const add = (a,b) => a+b;
+
+const list = [1,2,3,4,6]
+
+log(reduce(add, list))
+
