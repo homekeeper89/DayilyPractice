65057679c0a1e94f22cd5ecd71b2a13f8bc9468a
Homekeeper <homekeeper89@gmail.com>
2019-09-10 00:42:34 +0900

    [fix] @property의 사용
    기존의 quota는 멤버변수로 있었던 것이 @property와 함께 함수로 바꾸어졌다.
    그리고 quota는 그대로 부를수도 있고 자동으로 값이 set 되게 할 수도 잇다

--- basic/src/exe_class.py
+++ basic/src/exe_class.py
@@ -3,10 +3,29 @@ class Bucket(object):
     def __init__(self, period):
         self.period_delta = timedelta(seconds = period)
         self.reset_time = datetime.now()
-        self.quota = 0
+        self.max_quota = 0
+        self.quota_consumed = 0
     
     def __repr__(self):
-        return 'Bucker(quota=%d)' %self.quota
+        return 'Bucker(max_quota=%d, quota_consumed=%d)' \
+            %(self.max_quota, self.quota_consumed)
+
+    @property
+    def quota(self):
+        return self.max_quota - self.quota_consumed
+    
+    @quota.setter
+    def quota(self, amount):
+        delta = self.max_quota - amount
+        if amount == 0:
+            self.quota_consumed = 0
+            self.max_quota = 0
+        elif delta < 0:
+            assert self.quota_consumed == 0
+            self.max_quota = amount
+        else:
+            assert self.max_quota >= self.quota_consumed
+            self.quota_consumed += delta
 
 def fill(bucket, amount):
     now = datetime.now()
@@ -23,4 +42,21 @@ def deduct(bucket, amount):
         return False
     bucket.quota -= amount
     return True
+
+bucket = Bucket(60)
+print('Init', bucket)
+fill(bucket, 100)
+print('Filled', bucket)
 import ipdb; ipdb.set_trace()
+if deduct(bucket, 99):
+    print('Had 99 quota')
+else:
+    print('Not enough for 99 quota')
+print('Now', bucket)
+
+
+if deduct(bucket, 3):
+    print('Had 3 quota')
+else:
+    print('Not enough for 3 quota')
+print('Still', bucket)
