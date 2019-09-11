ce56eaccea1f76f8189b1a81cc166582aa44f091
김장훈 <homekeeper@gdnet.co.kr>
2019-09-11 16:34:50 +0900

    [fix] 새로운 형태로 class 가 변경
    이러헥 될경우 해당 클래스를 사용하고 있는 모든 부분에서 수정이 들어가야함
    obj.temperature -> obj.get_temperature()
    obj.temperature = val -> obj.set_temperature(val) 등 모든 부분에서 수정이 되어야함

--- basic/src/exe_property.py
+++ basic/src/exe_property.py
@@ -1,16 +1,25 @@
 class Celsius:
     def __init__(self, temperature=0):
-        self.temperature = temperature
+        self.set_temperature(temperature)
     
     def to_fahrenheit(self):
-        return (self.temperature * 1.8) + 32
+        return (self.get_temperature() * 1.8) + 32
+    
+    def get_temperature(self):
+        return self._temperature
+    
+    def set_temperature(self, value):
+        if value < -273:
+            raise ValueError('Temperature below -273 is not possible')
+        self._temperature = value
 
     def __repr__(self):
-        return 'temp {} fahr {}'.format(self.temperature, self.to_fahrenheit())
+        return 'temp {} fahr {}'.format(self._temperature, self.to_fahrenheit())
 
 if __name__ =='__main__':
-    man =Celsius()
-    man.temperature =37
-    print(man)
-    print(man.__dict__)
-    
+    c =Celsius(37)
+    c.get_temperature()
+    c.set_temperature(10)
+    print(c)
+    c._temperature = -300
+    print(c)
