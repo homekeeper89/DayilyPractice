c4aa8d76478104c798fc40c9c2ecbe6ba4490dd0
김장훈 <homekeeper@gdnet.co.kr>
2019-09-11 16:49:01 +0900

    [fix] temperature에 접근하는 경우 함수가 작동됨
    self.temperature에 고나여된 get, set 함수가 설정되서
    c.temperature를 통해서 모든 접근이 가능
    
    예를 들어 set 하는 로직이 바뀔 경우 새로운 함수로 해서 c.new_set_temp() 식이 아니라
    기존 방식 그대로 c.temperature = val로 하고 해당되는 로직만 추가해줘도 됨

--- basic/src/exe_property.py
+++ basic/src/exe_property.py
@@ -1,25 +1,29 @@
 class Celsius:
     def __init__(self, temperature=0):
-        self.set_temperature(temperature)
+        self.temperature = temperature
     
     def to_fahrenheit(self):
-        return (self.get_temperature() * 1.8) + 32
+        return (self.temperature * 1.8) + 32
     
     def get_temperature(self):
+        print('Getting Value')
         return self._temperature
     
     def set_temperature(self, value):
         if value < -273:
             raise ValueError('Temperature below -273 is not possible')
+        print('Setting value')
         self._temperature = value
+    # any code that retrieves the value of temperature will automatically
+    # call get_temperature() instead of a dictionay(__dict__) look-up.
+    # any code that assigns a value to temperature will automacatically call set_temperature()
+    # 얘가 어떻게 get, set으로...  property안에 순서대로인가?
+    temperature = property(get_temperature, set_temperature)
 
     def __repr__(self):
         return 'temp {} fahr {}'.format(self._temperature, self.to_fahrenheit())
 
 if __name__ =='__main__':
-    c =Celsius(37)
-    c.get_temperature()
-    c.set_temperature(10)
-    print(c)
-    c._temperature = -300
-    print(c)
+    c = Celsius()
+    c.temperature = 37
+    c.to_fahrenheit()
