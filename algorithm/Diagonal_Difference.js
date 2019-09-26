606cdb087fcd263fc1601460f4cd9daedbc6e5ab
Homekeeper <homekeeper89@gmail.com>
2019-09-26 23:35:14 +0900

    [new] 로직 개선했는데 결과가 신통치 않음
    테스트 자체 횟수가 너무 없다보니 사실 맞는지 아닌지 감도 안잡힘

--- algorithm/Diagonal_Difference.js
+++ algorithm/Diagonal_Difference.js
@@ -1,4 +1,5 @@
 //0925 Diagonal Difference
+// 6/11 test cases failed :(
 const log = console.log
 const reduce = (f, acc, iter)=>{
     if(!iter){
@@ -18,6 +19,7 @@ const arr = [11,2,4,
             10, 8, -12]
 // 0, 4, 8
 // 2, 4, 6
+const curry_go = curry(go)
 const arr2 = [[10,20,30,40],
               [10,20,30,40],
               [10,20,30,40],
@@ -25,17 +27,22 @@ const arr2 = [[10,20,30,40],
               // 0,5,10,15
               // 3,6,9,1
 const num = 4
-for(var i=0;i++;i<=2){
-  console.log("???")
-}          
-const f= ()=>{
+const f= (num)=> (iter) => {
   let si = 0
   let ei = num-1;
-  for(var i=0; i<=num; i++){
-    console.log(arr2[i][si], arr2[i][ei])
+  let siadd= 0;
+  let eiadd= 0;
+  for(var i=0; i<num; i++){
+    siadd += iter[i][si]
+    eiadd += iter[i][ei]
     si++;
     ei--;
-    console.log(si, ei)
   }
+  log(siadd, eiadd)
+  return siadd - eiadd
 }
-f()
+go(
+  arr2,
+  a => f(a.length)(a),
+  log
+  )
