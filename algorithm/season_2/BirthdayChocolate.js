5886a137791eb7c71993f20ea4455ee60abb8b98
Homekeeper <homekeeper89@gmail.com>
2019-10-08 00:39:40 +0900

    [new] 문제 준비
    주어진 배열(리스트)에서 정해진 갯수를 바탕으로 sum을 만드는 문제인것 같은데..
    좀 여러가지로 고민이 되긴 한다

---
+++ algorithm/season_2/BirthdayChocolate.js
@@ -0,0 +1,14 @@
+// 1008 
+const reduce = (f, acc, iter)=>{
+  if(!iter){
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  }
+  for(const ele of iter){
+    acc = f(acc, ele)
+  }
+  return acc;
+}
+const go = (...args) => reduce((a,f)=>f(a), args)
+const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs)
+const curry = f => (a, ..._) =>_.length ? f(a, ..._) : (..._) => f(a, ..._)
