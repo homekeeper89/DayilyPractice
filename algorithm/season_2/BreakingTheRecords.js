88be7c2787526e60e641a43f86bf75c3943e6bb5
김장훈 <homekeeper@gdnet.co.kr>
2019-10-15 23:46:24 +0900

    [fix] 기억 확인용 코드 작성
    회사일이 끝나야 다시 알고리즘을 시작할 수 있다..

--- algorithm/season_2/BreakingTheRecords.js
+++ algorithm/season_2/BreakingTheRecords.js
@@ -1,4 +1,4 @@
-// 1006 BreakingTheRecords
+// 1005 Breaking the Records
 const reduce = (f, acc, iter)=>{
   if(!iter){
     iter = acc[Symbol.iterator]();
@@ -9,52 +9,42 @@ const reduce = (f, acc, iter)=>{
   }
   return acc;
 }
-const go = (...args) => reduce((a,f)=>f(a), args)
-const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs)
-const curry = f => (a, ..._) => _.length ? f(a, ..._) : (..._)=>f(a, ..._)
-const curry_go = curry(go)
-const curry_reduce = curry(reduce)
-const log = console.log
-const test_input = [10,5,20,20,4,5,2,25,1]
-const test_expected = [2,4]
-let min_val=test_input[0]
-let max_val=test_input[0]
-let counter = [0,0]
 
+const go = (...args) =>reduce((a,f)=>f(a), args)
+const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs)
+const curry = f => (a, ..._) => _.length ? f(a, ..._) : (..._) => f(a, ..._)
+const curry_reduce = curry(reduce)
+const curry_go = curry(go)
+const curry_pipe = curry(pipe)
+const test_input = [10,5,20,20,4,5,2,25,1]
+QUnit.cases([{input:test_input, expected:[2,4]}])
+.test('first tset', function(params){
+  deepEqual(params.expected, params.expected)
+})
+let res = [0,0]
 const checker = (iter)=>{
   for(const ele of iter){
-    if(ele < min_val){
-      min_val = ele
-      counter[1] += 1
-    }else if(ele > max_val){
+    if(ele > max_val){
       max_val = ele
-      counter[0] += 1
+      res[0] += 1
+      continue
+    }else if(ele<min_val){
+      min_val = ele;
+      res[1] += 1
+      continue
     }
   }
-  return counter
+  return res
 }
-const filter = (f, iter)=>{
-  
-}
-const map = (f, iter)=>{
-  let res = []
-  for(const ele of iter){
-    res.push(f(ele))
-  }
-}
+let max_val = test_input[0]
+let min_val = test_input[0]
+const log = console.log
 const curry_checker = curry(checker)
-curry_go(
-  test_input, 
+curry_go(test_input,
   curry_checker(test_input),
   log
   )
-log("it is works test?", new Date())
-QUnit.test("First Test", function(assert){
-  assert.equal(true, 'a'==='a')
-})
-QUnit.test("First Test", function(assert){
-  assert.equal(true, 'a'==='a')
-})
-QUnit.test("First Test", function(assert){
-  assert.equal(true, 'a'==='a')
-})
+// n개를 받으면 1번째부터 n번째가지 슬라이싱
+// 1번째는 max이자 min으로 설정
+// filter를 통해 max, min 확인
+// 확인될때마다 카운트
--- algorithm/season_2/remember.js
+++ algorithm/season_2/remember.js
@@ -11,3 +11,19 @@ const reduce = (f, acc, iter)=>{
 const go = (...args) => reduce((a, f)=>f(a), args)
 const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs)
 const curry = f => (a, ..._) => _.length ? f(a, ..._):(..._)=>f(a, ..._)
+
+const map = (f, iter)=>{
+  let res = []
+  for(const ele of iter){
+    res.push(f(ele))
+  }
+  return res
+}
+
+const filter = (f, iter)=>{
+  let res = []
+  for(const ele of iter){
+    if(f(ele)) res.push(ele)
+  }
+  return res
+}
