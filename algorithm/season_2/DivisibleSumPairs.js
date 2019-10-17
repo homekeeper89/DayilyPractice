82fa56373500a175d1f7657f720fc46e7b1163bb
김장훈 <homekeeper89@gmail.com>
2019-10-18 00:48:19 +0900

    [new] DivisiblePairs 문제 준비
    의사코드는 대략적으로 구성되어있음. O(n^2)이라는게 문제.. 아니지 맞네

---
+++ algorithm/season_2/DivisibleSumPairs.js
@@ -0,0 +1,21 @@
+// 191018
+// array, k, array[i] < k && array[j] > k && arr[i] + arr[j] % k == 0
+// 이런 조건이라...
+// k보다 작은 애들, 큰애들 구분
+// 작은애들을 iter로 돌림
+// 큰애들과 하나씩 더하고 %k를 함
+// ==0 이면 push
+
+const reduce = (f, acc, iter)=>{
+  if(!iter){
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  }
+  for(const ele of iter){
+    acc = f(acc, ele)
+  }
+  return acc
+}
+const go = (...args) => reduce((a,f)=>f(a), args)
+const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs)
+const curry = f => (a, ..._) => _.length ? f(a, ..._) : (..._) => f(a, ..._)
