1c2959dde7a5225e01a843a8c2510f625f73f814
김장훈 <homekeeper@gdnet.co.kr>
2019-10-22 00:37:32 +0900

    [new] 의사코드 추가
    좀 더 정리해야함

--- algorithm/season_2/DivisibleSumPairs.js
+++ algorithm/season_2/DivisibleSumPairs.js
@@ -3,4 +3,9 @@
 // https://github.com/homekeeper89
 // input = [1,2,3,4,5,6], key = 5, res = [1,4],[2,3],[4,6]
 
+// 1. input array 를 iter
+// 2. key - iter 인덱스 값
+// 3. 2번 값이 리스트에 있는지 확인
+// 4. 있으면 res에 추가
+// 엔드조건은?
 
