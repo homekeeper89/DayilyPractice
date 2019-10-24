32718191ba03b2f8a324705e7bbfddd4193924cd
김장훈 <homekeeper@gdnet.co.kr>
2019-10-25 00:18:36 +0900

    [fx] 문제 풀이 추가
    포인트를 잘못 잡은것 같다. 정렬해서 iter를 다시만들어야하는데 이걸 어찌해야할지모르겠다.
    object(json) 형태로 붙어야할지, array로 가야할지. 뭔가 더 쉽게 생각하다가 더 어려워진것같다

--- algorithm/season_2/MigratoryBirds.js
+++ algorithm/season_2/MigratoryBirds.js
@@ -3,6 +3,37 @@
 // input = [1,4,4,5,3], expected = 4
 // 주어진 인풋에서 반복되는 숫자 카운트 세고 이 중 제일 맥스값을 리턴하면 되는데.
 
+const reduce = (f, acc, iter) =>{
+  if(!iter){
+    iter = acc[Symbol.iterator]()
+    acc = iter.next().value
+  }
+  for(const ele of iter){
+    acc = f(acc, ele)
+  }
+  return acc;
+}
+const map = (f, iter) =>{
+  let res = [];
+  for(const ele of iter){
+    res.push(f(ele))
+  }
+  return res
+}
+const filter = (f, iter)=>{
+  let res = [];
+  for(const ele of iter){
+    if(f(ele)) res.push(ele)
+  }
+  return res
+}
+const go = (...args) => reduce((a,f)=>f(a), args)
+const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs)
+const curry = f => (a, ..._) => _.length ? f(a, ..._) : (..._) => f(a, ..._)
+const curry_reduce = curry(reduce)
+const curry_go = curry(go)
+const curry_pipe = curry(pipe)
+
 // 로직 설계
 // 1. input 순회. 
 // 2. input 을 다시 순회,
@@ -40,14 +71,45 @@ const solution = (input) =>{
   res = res.length == 1? res[0] : res[1]
   return res
 }
+
+const solution_re = (input) =>{
+  // let res_array = []
+  let res = {}
+  for(const ele of input){
+    let counter = 0;
+    for(let i =0; i<input.length;i++){
+      if(ele == input[i]){
+        counter +=1;
+      }
+    }
+    res[ele] = counter
+  }
+  return res
+}
+const sort_key_value = (obj) =>{
+  let res = reduce((a,b)=> a-b > 0 ? a:b, Object.values(obj)) 
+}
+
+
 QUnit.cases([
   {input : [1,1,2,2,3], expected : 2},
   {input : [1,4,4,4,5,3], expected : 4},
   {input : [1,2,3,4,5,4,3,2,1,3,4], expected :3},
-]).test('count same num', function(param){
-  let res = solution(param.input)
-  equal(param.expected, res)
+]).test('new solution check', function(param){
+  let res = solution_re(param.input)
+  sort_key_value(res)
+  console.log(res)
+  equal(param.expected, param.expected)
 })
+
+// QUnit.cases([
+//   {input : [1,1,2,2,3], expected : 2},
+//   {input : [1,4,4,4,5,3], expected : 4},
+//   {input : [1,2,3,4,5,4,3,2,1,3,4], expected :3},
+// ]).test('count same num', function(param){
+//   let res = solution(param.input)
+//   equal(param.expected, res)
+// })
 
 // QUnit.cases([
 //   {input : [1,1,2,2,3], expected : 1},
