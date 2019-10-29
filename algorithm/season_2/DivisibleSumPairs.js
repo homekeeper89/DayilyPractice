b33d21a6e6a9b6d7ff657d4f1129b9fae0910356
김장훈 <homekeeper@gdnet.co.kr>
2019-10-29 17:41:51 +0900

    [fix] 충돌 수정 했는데, 이거 또 충돌날거같은데

--- algorithm/season_2/DivisibleSumPairs.js
+++ algorithm/season_2/DivisibleSumPairs.js
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-// 191018
-// array, k, array[i] < k && array[j] > k && arr[i] + arr[j] % k == 0
-// 이런 조건이라...
-// k보다 작은 애들, 큰애들 구분
-// 작은애들을 iter로 돌림
-// 큰애들과 하나씩 더하고 %k를 함
-// ==0 이면 push
-
-const reduce = (f, acc, iter)=>{
-  if(!iter){
-    iter = acc[Symbol.iterator]();
-    acc = iter.next().value;
-  }
-  for(const ele of iter){
-    acc = f(acc, ele)
-  }
-  return acc
-}
-const map = (f, iter)=>{
-  let res =[];
-  for(const ele of iter){
-    res.push(f(ele))
-  }
-  return res;
-}
-const filter = (f, iter)=>{
-  let res =[];
-  for(const ele of iter){
-    if(f(ele)) res.push(ele)
-  }
-  return res
-}
-const go = (...args) => reduce((a,f)=>f(a), args)
-const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs)
-const curry = f => (a, ..._) => _.length ? f(a, ..._) : (..._) => f(a, ..._)
-=======
 // 1022 Divisible Sum Pairs
 // written by homekeeper89
 // https://github.com/homekeeper89
@@ -69,4 +32,3 @@ QUnit.cases(
   equal(res, param.expected)
 })
 
->>>>>>> 6f598cd0b3dca1c89e132e7acafafb0f93c4a482
