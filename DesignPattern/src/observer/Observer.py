5cf5fb9c240372f0e0ab2f0f377e21e586c8addd
matthew <homekeeper89@gmail.com>
2019-12-21 14:06:07 +0900

    [new] 뉴스 구독, 제일 최상위 NewPublisher

--- DesignPattern/src/observer/Observer.py
+++ DesignPattern/src/observer/Observer.py
@@ -1,3 +1,4 @@
+# 서브젝트는 옵저버를 관리한다.
 class Subject:
     def __init__(self):
         self.__observers = []
@@ -9,7 +10,7 @@ class Subject:
         for observer in self.__observers:
             observer.notify(self, *args, **kwargs)
 
-
+# 옵저버는 서브젝트를 감시하는 객체를 위한 인터페이스를 제공 : 여기서는 notify
 class Observer1:
     def __init__(self, subject):
         subject.register(self)
@@ -22,7 +23,7 @@ class Observer2:
         subject.register(self)
 
     def notify(self, subject, *args):
-        print(type(self).__name__, ':: GOt', args, 'from', subject)
+        print(type(ㅎself).__name__, ':: GOt', args, 'from', subject)
 
 if __name__ == '__main__':
     subject = Subject()
@@ -30,3 +31,5 @@ if __name__ == '__main__':
     observer2 = Observer2(subject)
 
     subject.notifyAll('notification')
+
+# ConcreateObserver는 observer 인터페이스를 구현해 자신의 subject에 등록한다.
---
+++ DesignPattern/src/observer/news/readme.txt
@@ -0,0 +1,11 @@
+뉴스 에이전시 옵저버 패턴으로 구현하자
+
+뉴스 에이전시는 실시간 뉴스를 신속하게 구독자에게 전달해야한다.
+구독자는 신문 뿐만 아니라 이메일, 문자, 음성 메시지 등 다양한 방식으로 뉴스를 전달 받는다.
+즉 추후에 새로운 행태의 매체도 지원 할 수 있도록 설계해야 한다.
+
+- NewsPublisher 클래스에 구현한다
+- NewsPublisher는 구독자가 구현할 인터페이스를 제공한다
+- Observer는 attach() 메소드를 통해 자신을 NewsPublisher에 등록하고 detach() 메소드로 등록을 취소한다
+- subscribers() 는 Subject에 등록된 구독자 목록을 반환한다.
+- notifySubscriber()는 NewsPublihser에 등록된 모든 구독자에게 알림을 보낸다.
